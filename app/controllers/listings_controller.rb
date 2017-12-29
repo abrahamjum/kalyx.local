6a743f5cd9ff5598ed8580682a66359daa385945
abrahamjum <abrahamjum@gmail.com>
2017-12-29 08:28:13 -0800

    Merge

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,24 @@
+class ListingsController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  def create
+  end
+  
+end
